64ae2323093dc1d7a636ca76fbf29f6eca501806
natalie-macpherson <macphersonnatalie@gmail.com>
2015-02-09 18:50:31 -0500

    setting up html/jc and css file
    setting up var in jc

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var blueberries = {
+	veriety: "Vaccinium corymbosum",
+	growsOnShrub: true,
+	energy: 240,
+	carbohydrates: 14.49,
+	protein: 0.74,
+};
+
+var grapes = {
+	veriety: "Vitis vinifera",
+	growsOnShrub: false,
+	energy: 288,
+	carbohydrates: 18.1,
+	protein: 0.72,
+};
+
+var redCurrent = {
+	veriety: "Ribes rubrum",
+	growsOnShrub: true,
+	energy: 234,
+	carbohydrates: 13.8,
+	protein: 1.4,
+};
